49756e2ac3058e8ec930fbcc4d5b5a018f3847be
ngriebel1 <159309689+ngriebel1@users.noreply.github.com>
2024-03-11 10:42:33 -0700

    Create CIS129_03-11-24_CodeAlong.py
    + Added code along for 3-11-24 codework

---
+++ CIS129_03-11-24_CodeAlong.py
@@ -0,0 +1,19 @@
+# In-class code-along for CIS 129
+# Written on 03-11-2024
+# By Nick Griebel
+#
+# This program is practice with loops in python
+
+doAgain = 'y'
+
+while (doAgain == 'y'):
+    number = int(input("Please enter a number: "))
+
+    if (number % 2 == 0):
+        print("This number is even")
+    else:
+        print("This number is odd")
+
+    doAgain = input("Would you like to enter another number? (Y/N): ")
+    while (doAgain != 'y' and doAgain != 'n'):
+        doAgain = input("What? Respond with y or n: ")
