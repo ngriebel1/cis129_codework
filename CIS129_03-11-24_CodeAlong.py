31406103f1d99b4f9bfca8f277f1a2a9ac201d0b
ngriebel1 <159309689+ngriebel1@users.noreply.github.com>
2024-03-11 10:45:38 -0700

    Update CIS129_03-11-24_CodeAlong.py
    + 03-11-24(10:45AM)  Updated code-along source with a main routine and a definition of the __main__ variable to the main() routine

--- CIS129_03-11-24_CodeAlong.py
+++ CIS129_03-11-24_CodeAlong.py
@@ -4,16 +4,18 @@
 #
 # This program is practice with loops in python
 
-doAgain = 'y'
+def main():
+    doAgain = 'y'
+    while (doAgain == 'y'):
+        number = int(input("Please enter a number: "))
 
-while (doAgain == 'y'):
-    number = int(input("Please enter a number: "))
+        if (number % 2 == 0):
+            print("This number is even")
+        else:
+            print("This number is odd")
 
-    if (number % 2 == 0):
-        print("This number is even")
-    else:
-        print("This number is odd")
+        doAgain = input("Would you like to enter another number? (Y/N): ")
+        while (doAgain != 'y' and doAgain != 'n'):
+            doAgain = input("What? Respond with y or n: ")
 
-    doAgain = input("Would you like to enter another number? (Y/N): ")
-    while (doAgain != 'y' and doAgain != 'n'):
-        doAgain = input("What? Respond with y or n: ")
+__main__ = main()
