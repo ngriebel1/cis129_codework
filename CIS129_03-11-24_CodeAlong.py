1b3e63ec4b9315b21e2b40e24e5c519d0546a431
ngriebel1 <159309689+ngriebel1@users.noreply.github.com>
2024-03-13 09:53:12 -0700

    Update CIS129_03-11-24_CodeAlong.py
    + Code replaced with updated version, written on 03-13-2024 by Nick Griebel for CIS 129

--- CIS129_03-11-24_CodeAlong.py
+++ CIS129_03-11-24_CodeAlong.py
@@ -1,21 +1,46 @@
-# In-class code-along for CIS 129
-# Written on 03-11-2024
-# By Nick Griebel
+# This program explores looping and input checking in python
+# Written by Nick Griebel
+# On 03-13-2024
+# For CIS 129
 #
-# This program is practice with loops in python
 
 def main():
-    doAgain = 'y'
-    while (doAgain == 'y'):
-        number = int(input("Please enter a number: "))
+    restartInput = "y"
+    userNumber = ""
 
-        if (number % 2 == 0):
-            print("This number is even")
+    # Call get_integer to retrieve number from the user
+    while (restartInput == "y"):
+        userNumber = get_integer("Please enter a number: ")
+
+        # Check whether users inputted number is even or odd
+        if (userNumber % 2 == 0):
+            print("Your number", userNumber, "is even!")
         else:
-            print("This number is odd")
+            print("Your number", userNumber, "is odd!")
 
-        doAgain = input("Would you like to enter another number? (Y/N): ")
-        while (doAgain != 'y' and doAgain != 'n'):
-            doAgain = input("What? Respond with y or n: ")
+        # Ask user if they would like to enter another number
+        restartInput = input("Would you like to enter another number? (Y/N): ")
 
-__main__ = main()
+        # Check for correct (y/n) input from user. repeat input if incorrect input
+        if (restartInput != "y" and restartInput != "n"):
+           while (restartInput != "y" and restartInput != "n"):
+               restartInput = input("Would you like to enter another number? (Y/N): ")
+
+        # If user entered no(n), return from main to caller.
+        if (restartInput.lower() == "n"):
+            return 0
+
+# This routine retrieves a number from the user and verifies
+#   that input was an integer. If it was not, an error is thrown
+#   and user is prompted to re-enter input
+def get_integer(message):
+    while True:
+
+        try:
+            user_input = int(input(message))
+            return user_input
+        except ValueError:
+            print("Incorrect data entered. Please re-attempt")
+
+
+__main__ = main()       # Set our main routine as main()
